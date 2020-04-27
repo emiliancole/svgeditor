8d70544fe6019eae1708c12220b39740f57d199d
Emilian Cole <emiliancole@gmail.com>
2020-04-27 19:46:44 +0200

    Create svgShapeDefs.js

---
+++ svgShapeDefs.js
@@ -0,0 +1,12 @@
+const STYLE0 = 'stroke:black;stroke-width:1;fill:none;'; //STYLErgb = STYLE111
+const STYLE1 = 'stroke:black;stroke-width:1;fill:red;';
+const STYLE01 = 'stroke:black;stroke-width:1;fill:green;';
+const STYLE001 = 'stroke:black;stroke-width:1;fill:blue;';
+
+var circle = "<circle cx='100' cy='50' r='10' style="+STYLE0+" />\n";
+var ellipse = "<ellipse cx='100' cy='50' rx='10' ry='5' style="+STYLE0+" />\n";
+
+var line = "<line x1='100' y1='50' x2='200' y2='100' style="+STYLE0+" />\n";
+var rect = "<rect x='100' y='50' width='100' height='50' style="+STYLE0+" />\n";
+var path = "<path d='M 10,10 H 90 V 90' style="+STYLE0+" />\n";
+var svg = "<svg width='500' height='500' viewBox='0 0 500 500'>\n\n\n</svg>";
